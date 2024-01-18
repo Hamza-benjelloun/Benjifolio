d0008f36d4d7dcdf65daffc87fbe958517e5bd7c
Hamza-benjelloun <hamza.benjelloun@octo.ma>
2024-01-18 21:47:16 +0100

    Recreating the old portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
-
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
@@ -16,13 +15,13 @@ const App = () => {
         <Tech />
         <Works />
         <Feedbacks />
-        <div className="relative z-0">
+        <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
         </div>
       </div>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
